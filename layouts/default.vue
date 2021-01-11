d45fa903853538af9cb719a91d039b9798b71961
adshidtadka <nextage900fx@gmail.com>
2021-01-11 23:48:58 +0900

    feat: html, cssのコーディング

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -24,94 +12,74 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      <!-- <v&#45;btn icon @click.stop="miniVariant = !miniVariant"> -->
+      <!--   <v&#45;icon>mdi&#45;{{ `chevron&#45;${miniVariant ? 'right' : 'left'}` }}</v&#45;icon> -->
+      <!-- </v&#45;btn> -->
+      <!-- <v&#45;btn -->
+      <!--   icon -->
+      <!--   @click.stop="clipped = !clipped" -->
+      <!-- > -->
+      <!--   <v&#45;icon>mdi&#45;application</v&#45;icon> -->
+      <!-- </v&#45;btn> -->
+      <!-- <v&#45;btn icon @click.stop="fixed = !fixed"> -->
+      <!--   <v&#45;icon>mdi&#45;minus</v&#45;icon> -->
+      <!-- </v&#45;btn> -->
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <!-- <v&#45;btn -->
+      <!--   icon -->
+      <!--   @click.stop="rightDrawer = !rightDrawer" -->
+      <!-- > -->
+      <!--   <v&#45;icon>mdi&#45;menu</v&#45;icon> -->
+      <!-- </v&#45;btn> -->
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <!-- <v&#45;navigation&#45;drawer v&#45;model="rightDrawer" :right="right" temporary fixed> -->
+    <!--   <v&#45;list> -->
+    <!--     <v&#45;list&#45;item @click.native="right = !right"> -->
+    <!--       <v&#45;list&#45;item&#45;action> -->
+    <!--         <v&#45;icon light>mdi&#45;repeat</v&#45;icon> -->
+    <!--       </v&#45;list&#45;item&#45;action> -->
+    <!--       <v&#45;list&#45;item&#45;title>Switch drawer (click me)</v&#45;list&#45;item&#45;title> -->
+    <!--     </v&#45;list&#45;item> -->
+    <!--   </v&#45;list> -->
+    <!-- </v&#45;navigation&#45;drawer> -->
+    <v-footer :absolute="!fixed" app>
+      <span>House hunting &copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Let's hunt",
+          to: "/",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          icon: "mdi-chart-bubble",
+          title: "About",
+          to: "/about",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+      title: "House hunting",
+    };
+  },
+};
 </script>
---
+++ pages/room.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="mx-auto" max-width="744">
+        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+        <v-card-title>{{ room.name }}</v-card-title>
+        <v-card-subtitle>{{ room.station_name }}, {{ room.route_name }}</v-card-subtitle>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      room: {
+        name: "beautiful beautiful house",
+        station_name: "koenji",
+        route_name: "JR sobu line",
+      },
+    };
+  },
+};
+</script>
+
