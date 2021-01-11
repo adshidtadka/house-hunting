41e15e4492e0b1cca0cddf53609f12232d1da5af
adshidtadka <nextage900fx@gmail.com>
2021-01-12 02:04:47 +0900

    add: /roomsのpathを作成

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,10 +3,11 @@ import App from './app';
 import AuthRoute from './routes/auth.route';
 import IndexRoute from './routes/index.route';
 import UsersRoute from './routes/users.route';
+import RoomsRoute from './routes/rooms.route';
 import validateEnv from './utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
+const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new RoomsRoute()]);
 
 app.listen();
